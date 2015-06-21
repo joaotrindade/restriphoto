62be6848b056c49e46a865487c8c386f0eb25c45
joaotrindade <joao.ferreira.trindade@gmail.com>
2015-06-21 02:23:48 +0100

    - stringify

--- client/js/controller.js
+++ client/js/controller.js
@@ -368,7 +368,7 @@ app.controller('userController', function($scope,$routeParams,$cookies,$window)
 		$.ajax({
 			url : apiurl,
 			type: "POST",
-			data : {'"condicoes"' : JSON.stringify(obj)},
+			data : {'"condicoes"' : obj},
 			contentType : 'application/json',
 			success: function(response){
 				console.log(response);
